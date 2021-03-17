5b719ef6a78a7763f9647d394015bd3f990c8e0e
jvmr535 <46688620+jvmr535@users.noreply.github.com>
2021-03-17 15:56:59 -0300

    feat: :sparkles: Aula 23

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -10,67 +10,87 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 20,
-                ),
-                Container(
-                  width: 200,
-                  height: 200,
-                  child: Image.asset('assets/images/logo.jpg'),
-                ),
-                TextField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
+  Widget _body(BuildContext context) {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 20,
                   ),
-                  onChanged: (text) {
-                    email = text;
-                  },
-                ),
-                Container(
-                  height: 20,
-                ),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
+                  Container(
+                    width: 200,
+                    height: 200,
+                    child: Image.asset('assets/images/logo.jpg'),
                   ),
-                  onChanged: (text) {
-                    password = text;
-                  },
-                ),
-                Container(
-                  height: 20,
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    if (email == 'jvmr535@gmail.com' &&
-                        password == 'joao1234') {
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    } else {
-                      print('errado');
-                    }
-                  },
-                  child: Text('Entrar'),
-                )
-              ],
+                  TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (text) {
+                      email = text;
+                    },
+                  ),
+                  Container(
+                    height: 20,
+                  ),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (text) {
+                      password = text;
+                    },
+                  ),
+                  Container(
+                    height: 20,
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      if (email == 'jvmr535@gmail.com' &&
+                          password == 'joao1234') {
+                        Navigator.of(context).pushReplacementNamed('/home');
+                      } else {
+                        print('errado');
+                      }
+                    },
+                    child: Text('Entrar'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/images/background.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(color: Colors.black.withOpacity(0.3)),
+          _body(context),
+        ],
       ),
     );
   }
