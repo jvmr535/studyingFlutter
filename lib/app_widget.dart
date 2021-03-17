23e756ed434623621dc1f0b2aecfed6af166f0f1
jvmr535 <46688620+jvmr535@users.noreply.github.com>
2021-03-17 10:05:52 -0300

    feat: :sparkles: Aula 12

---
+++ lib/app_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/app_controller.dart';
+
+import 'Home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: AppController.instance,
+        builder: (context, child) {
+          return MaterialApp(
+            theme: ThemeData(
+                primarySwatch: Colors.red,
+                brightness: AppController.instance.isDarkTheme
+                    ? Brightness.dark
+                    : Brightness.light),
+            home: HomePage(),
+          );
+        });
+  }
+}
