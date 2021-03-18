8f6aa0fc0e8f9fcacbbe2c6f66d3b32dd13a75bd
jvmr535 <46688620+jvmr535@users.noreply.github.com>
2021-03-18 11:06:42 -0300

    feat: :sparkles: Aula 25

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -15,6 +15,34 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: Image.network(
+                  'https://instagram.fbhz4-1.fna.fbcdn.net/v/t51.2885-19/s150x150/91164021_149784546340120_1732440819085869056_n.jpg?tp=1&_nc_ht=instagram.fbhz4-1.fna.fbcdn.net&_nc_ohc=33yUk0_wZ-sAX-KeytM&ccb=7-4&oh=e2d1a239485fac306b6ee28a00fc145c&oe=607B8485'),
+              accountName: Text('João Vitor'),
+              accountEmail: Text('jvmr535@gmail.com'),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Inicio'),
+              subtitle: Text('Tela de inicio'),
+              onTap: () {
+                print('Home');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('Sair'),
+              subtitle: Text('Finalizar sessão'),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/');
+              },
+            )
+          ],
+        ),
+      ),
       appBar: AppBar(
         title: Text('Home Page'),
         actions: [CustomSwitch()],
